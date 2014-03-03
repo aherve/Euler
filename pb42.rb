420c0e43ae45b41b11e4feb774bd5f8202f4fe3f
Aurélien <mail@aurelien-herve.com>
2014-03-03 10:59:39 +0100

    Update pb42.rb

--- pb42.rb
+++ pb42.rb
@@ -7,6 +7,6 @@ is_triangle = -> x {
   (r = 0.5*( Math.sqrt(1+8*x) - 1)) == r.to_i
 }
 
-is_valid = -> x { is_triangle.call( code.call( x)) }
+is_valid = -> x { is_triangle.(code.(x)) }
 
 puts File.read("./words.txt").gsub('"','').split(",").select(&is_valid).size
